c42d3260811153f33e0d108eeb6dc07262983865
rolandogr2305 <rolandogr2305@gmail.com>
2026-02-27 00:25:50 -0600

    Delete analysis.py

--- analysis.py
+++
@@ -1,5 +0,0 @@
-#Cargamos nuestro archivo CSV
-df = pd.read_csv("peliculas.csv")
-# Eliminamos filas con datos nulos importantes
-df = df.dropna(subset=["titulo", "anio", "genero", "calificacion", "votos"])
-df.head()
