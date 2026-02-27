96c24001d0edcda003d4d26c333d2d5dcd818b42
rolandogr2305 <rolandogr2305@gmail.com>
2026-02-27 00:29:40 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 #Cargamos nuestro archivo CSV
-df = pd.read_csv("peliculas.csv")
+df = pd.read_csv("datosPeliculas.csv")
 # Eliminamos filas con datos nulos importantes
 df = df.dropna(subset=["titulo", "anio", "genero", "calificacion", "votos"])
 df.head()
