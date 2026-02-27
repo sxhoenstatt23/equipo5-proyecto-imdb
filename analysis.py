64de5cbc2a030f45e549c51623e8ba52c4eefc8c
rolandogr2305 <rolandogr2305@gmail.com>
2026-02-27 00:24:15 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,3 +3,16 @@ df = pd.read_csv("peliculas.csv")
 # Eliminamos filas con datos nulos importantes
 df = df.dropna(subset=["titulo", "anio", "genero", "calificacion", "votos"])
 df.head()
+#Filtramos por un genero en especifico
+genero = "Terror"
+df_genero = df[df["genero"].str.contains(genero)]
+df_genero.head()
+#Calculamos el promedio de la calificacion por anio de estas
+promedio_por_año = df_genero.groupby("anio")["calificacion"].mean()
+#Creamos una grafica para mostrar la anterior informacion
+plt.figure()
+promedio_por_año.plot()
+plt.title("Promedio de calificación por año (Género: Terror)")
+plt.xlabel("Año")
+plt.ylabel("Calificación promedio")
+plt.show()
